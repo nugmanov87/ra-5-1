99c33de6d80b8ebe305a0b3d92dd13f07b77f820
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2020-04-09 08:33:01 +0500

    удалены ненужные компоненты

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Card = props => {
-  return <div className="ui card">{props.children}</div>;
-};
+export default function Card(props) {
+  const { title, text } = props;
+  return (
+    <div
+      className="ui card"
+      style={({ width: `${15}rem` }, { marginBottom: `${70}px` })}
+    >
+      {props.children}
+      <div className="card-body">
+        <h2 className="header" style={{ margin: `${20}px` }}>
+          {title}
+        </h2>
+        <div className="description" style={{ margin: `${20}px` }}>
+          {text}
+        </div>
+        <a href="#" className="ui primary button" style={{ margin: `${20}px` }}>
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+}
 
 Card.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.element),
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
 };
-
-export default Card;
